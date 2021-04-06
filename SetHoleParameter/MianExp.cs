f79d439cd90b04297328f13519e4b14c1174758b
ycchen10 <ycchen10@CDUW4CV733W73B>
2021-04-06 15:12:05 +0800

    添加项目文件。

---
+++ SetHoleParameter/MianExp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NXOpen;
+using NXOpen.UIStyler;
+using NXOpen.UF;
+
+namespace SetHoleParameter
+{
+    public class MianExp
+    {
+        public static int Main(string[] args)
+        {
+
+            Tag[] edges = { (Tag)59752 };
+            SetHoleParameter hole = new SetHoleParameter(@"C:\Users\ycchen10\OneDrive - kochind.com\Desktop\MolexPlugIn-1899\hole\");
+            hole.SetCenterHole("DRILLING", edges, 1.0);
+            return 0;
+        }
+
+
+
+        /// <summary>
+        /// 卸载函数
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static int GetUnloadOption(string arg)
+        {
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.Explicitly);
+            return System.Convert.ToInt32(Session.LibraryUnloadOption.Immediately);
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.AtTermination);
+        }
+    }
+}
---
+++ SetHoleParameter/SetHoleParameter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using NXOpen;
+using NXOpen.UF;
+using NXOpen.Utilities;
+
+namespace SetHoleParameter
+{
+
+    public class SetHoleParameter
+    {
+        [DllImport("DrillParameterForGrip.dll", EntryPoint = "set_hole_parameter")]
+        internal static extern int set_hole_parameter(string gripPath, Tag[] edges, string operName, double arr, double dpth, double clr);
+        string _file;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gripFile">文件夹地址需要加\</param>
+        public SetHoleParameter(string gripFile)
+        {
+            this._file = gripFile;
+        }
+        /// <summary>
+        /// 中心孔
+        /// </summary>
+        /// <param name="edges"></param>
+        /// <param name="dpth"></param>
+        /// <returns></returns>
+        public bool SetCenterHole(string operName, Tag[] edges, double dpth)
+        {
+            JAM.StartUFCall();
+            int start = set_hole_parameter(_file + "Standard_Hole.grx",  edges, operName, edges.Length, dpth, 5.0);
+            JAM.EndUFCall();
+            if (start == 0)
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
